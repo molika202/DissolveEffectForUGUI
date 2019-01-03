c56e6fbf7c583efd684b76044ad6e1812cd80314
molika202 <molika202@163.com>
2019-01-03 10:22:48 +0800

    lee
    增加叠牌动画

---
+++ Assets/Resources/Scripts/GameUI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUI : MonoBehaviour {
+
+    const byte num = 52;
+    public GameObject cardItem;
+    public Transform cardParent;
+	void Start () {
+        StartCoroutine(ShowAnimCard());
+	}
+
+    void InitUI()
+    {
+
+    }
+    IEnumerator ShowAnimCard()
+    {
+        Sprite tx = Resources.Load<Sprite>("Texture/f002");
+        for (byte i = 0; i < num; i++)
+        {
+            yield return new WaitForSecondsRealtime(0.05f);
+            GameObject item = Instantiate(cardItem, cardParent);
+            item.name = i.ToString();
+            item.GetComponent<Image>().sprite = tx;
+            item.transform.localRotation = Quaternion.identity;
+            item.transform.localScale = Vector3.one;
+            item.transform.localPosition = new Vector3(i + 1, 0);
+        }
+
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
